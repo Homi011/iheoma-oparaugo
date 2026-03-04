ecf175e46c8a292c96c098b8a9c6f1913eaa9ef9
Homi011 <iheoma.oparaugo1@gmail.com>
2026-03-04 13:26:12 +0100

    Remove underline from logo

--- app/components/LogoNavy.jsx
+++ app/components/LogoNavy.jsx
@@ -1,13 +1,12 @@
 export default function LogoNavy() {
     return (
-      <svg width="210" height="48" viewBox="0 0 210 48" xmlns="http://www.w3.org/2000/svg">
+      <svg width="210" height="36" viewBox="0 0 210 36" xmlns="http://www.w3.org/2000/svg">
         <text x="0" y="26" fontFamily="Montserrat, sans-serif" fontWeight="700" fontSize="22" fill="#003366">
           Iheoma
         </text>
         <text x="88" y="26" fontFamily="Montserrat, sans-serif" fontWeight="500" fontSize="22" fill="#003366">
           Oparaugo
         </text>
-        <line x1="88" y1="34" x2="212" y2="34" stroke="#888888" strokeWidth="1"/>
       </svg>
     );
   }
--- app/components/LogoWhite.jsx
+++ app/components/LogoWhite.jsx
@@ -1,13 +1,12 @@
 export default function LogoWhite() {
     return (
-      <svg width="210" height="48" viewBox="0 0 210 48" xmlns="http://www.w3.org/2000/svg">
+      <svg width="210" height="36" viewBox="0 0 210 36" xmlns="http://www.w3.org/2000/svg">
         <text x="0" y="26" fontFamily="Montserrat, sans-serif" fontWeight="700" fontSize="22" fill="#ffffff">
           Iheoma
         </text>
         <text x="88" y="26" fontFamily="Montserrat, sans-serif" fontWeight="500" fontSize="22" fill="#ffffff">
           Oparaugo
         </text>
-        <line x1="88" y1="34" x2="212" y2="34" stroke="rgba(255,255,255,0.6)" strokeWidth="1"/>
       </svg>
     );
   }
