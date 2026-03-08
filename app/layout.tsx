7795df476e1f94d6bf12808c51c904b4dbca3abd
Homi011 <iheoma.oparaugo1@gmail.com>
2026-03-08 14:43:31 +0100

    Add favicon link tags to head

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,6 +27,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <head>
+      <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <script async src="https://www.googletagmanager.com/gtag/js?id=G-5N08Y0PTTC" />
         <script dangerouslySetInnerHTML={{ __html: `window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','G-5N08Y0PTTC');` }} />
       </head>
